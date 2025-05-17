c93313080506f44b334488a3de519d0833e87696
Ashish <am7011414408@gmail.com>
2025-05-17 22:17:15 +0530

    initial commint

---
+++ Frontend/src/components/BestSeller.jsx
@@ -0,0 +1,36 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+const BestSeller = () => {
+    const { products } = useContext(ShopContext);
+
+    const [bestSeller, setBestSeller] = useState([]);
+
+    useEffect(() => {
+        const bestProduct = products.filter((item) => item.bestseller);
+        setBestSeller(bestProduct.slice(0, 5));
+    }, [products]);
+
+    return (
+        <div className='my-10'>
+            <div className='text-center text-3xl py-8'>
+                <Title text1={'BEST'} text2={'SELLERS'} />
+                <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                    Step into the New Year with Style!
+                    Discover our latest collection of trendy and comfortable clothing designed to elevate your wardrobe. From chic casuals to statement pieces, each outfit blends modern aesthetics with timeless elegance. Perfect for every occasion, our collection features vibrant colors, premium fabrics, and versatile designs that cater to your unique style. Shop now and redefine your fashion game!
+                </p>
+            </div>
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
+                {
+                    bestSeller.map((item, index) => (
+                        <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price} />
+                    ))
+                }
+            </div>
+        </div>
+    );
+};
+
+export default BestSeller;
