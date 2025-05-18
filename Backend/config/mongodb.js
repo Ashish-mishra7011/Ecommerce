753d2151d6b09e45c77939f322bf1d9c462b00cf
Ashish <am7011414408@gmail.com>
2025-05-18 14:13:05 +0530

    changes done 1

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -3,13 +3,17 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
 
-    mongoose.connection.on('connected', () => {
+    
+    try {
+        mongoose.connection.on('connected', () => {
         console.log("Database Connected");
 
     })
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
-
+        await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
 
 export default connectDB
